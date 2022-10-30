24de578740af716905bb02c30b08cb03bae59cd8
Robert Salmon <robertsalmon88@gmail.com>
2022-10-30 08:57:08 -0500

    fixed project

--- src/components/homeScreen/Married.jsx
+++ src/components/homeScreen/Married.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import { Parallax } from "react-parallax";
-import marriedVid from "./vids/married.mp4";
+// import marriedVid from "./vids/married.mp4";
 
 const Married = () => (
-  <Parallax className="image" bgImage={marriedVid} strength={800}>
+  <Parallax className="image" strength={800}>
     <div className="content">
       <span className="img-txt">Newly Wed</span>
     </div>
--- src/components/homeScreen/Vid3.jsx
+++ src/components/homeScreen/Vid3.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { Parallax } from "react-parallax";
-import vid3 from "./vids/vid3.mp4"
+// import vid3 from "./vids/vid3.mp4"
 
 
 const Vid3 = () => (
-  <Parallax className="image" bgImage={vid3} strength={800}>
+  <Parallax className="image" strength={800}>
     <div className="content">
       <span className="img-txt">Blah Blah</span>
     </div>
